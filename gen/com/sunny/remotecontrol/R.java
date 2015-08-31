d6edee7a61eb69a1cfb3588f1de8be5d60b3bb49
Zhou Zejin <zjzhou1@chinaums.com>
2015-08-31 20:15:00 +0800

    完成 24.1 创建RemoteControl项目

--- gen/com/sunny/remotecontrol/R.java
+++ gen/com/sunny/remotecontrol/R.java
@@ -25,8 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080007;
         public static final int fragment_container=0x7f080000;
+        public static final int fragment_remote_control_enterButton=0x7f080006;
+        public static final int fragment_remote_control_oneButton=0x7f080005;
+        public static final int fragment_remote_control_selectedTextView=0x7f080002;
+        public static final int fragment_remote_control_tableLayout=0x7f080001;
+        public static final int fragment_remote_control_workingTextView=0x7f080003;
+        public static final int fragment_remote_control_zeroButton=0x7f080004;
     }
     public static final class layout {
         public static final int activity_fragment=0x7f030000;
@@ -36,9 +42,8 @@ public final class R {
         public static final int remote_control=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
--- src/com/sunny/remotecontrol/RemoteControlFragment.java
+++ src/com/sunny/remotecontrol/RemoteControlFragment.java
@@ -1,7 +1,67 @@
 package com.sunny.remotecontrol;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RemoteControlFragment extends Fragment {
+
+	private TextView mSelectedTextView;
+	private TextView mWorkingTextView;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_remote_control, container, false);
+		
+		mSelectedTextView = (TextView) view.
+				findViewById(R.id.fragment_remote_control_selectedTextView);
+		mWorkingTextView = (TextView) view.
+				findViewById(R.id.fragment_remote_control_workingTextView);
+		
+		View.OnClickListener numberButtonListener = new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				TextView textView = (TextView)v;
+				String working = mWorkingTextView.getText().toString();
+				String text = textView.getText().toString();
+				if (working.equals("0")) {
+					mWorkingTextView.setText(text);
+				} else {
+					mWorkingTextView.setText(working + text);
+				}
+			}
+		};
+		
+		Button zeroButton = (Button) view
+				.findViewById(R.id.fragment_remote_control_zeroButton);
+		zeroButton.setOnClickListener(numberButtonListener);
+		
+		Button oneButton = (Button) view
+				.findViewById(R.id.fragment_remote_control_oneButton);
+		oneButton.setOnClickListener(numberButtonListener);
+		
+		Button enterButton = (Button) view
+				.findViewById(R.id.fragment_remote_control_enterButton);
+		enterButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				CharSequence working = mWorkingTextView.getText();
+				if (working.length() > 0) {
+					mSelectedTextView.setText(working);
+				}
+				mWorkingTextView.setText("0");
+			}
+		});
+		
+		return view;
+	}
 
 }

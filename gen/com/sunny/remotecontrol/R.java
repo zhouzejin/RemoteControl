cc9cefd4a0d3a1fb4f676ebd42758cffd5993c7e
Zhou Zejin <zjzhou1@chinaums.com>
2015-09-01 10:21:26 +0800

    完成第24章 样式与include标签的使用

--- gen/com/sunny/remotecontrol/R.java
+++ gen/com/sunny/remotecontrol/R.java
@@ -25,18 +25,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080004;
         public static final int fragment_container=0x7f080000;
-        public static final int fragment_remote_control_enterButton=0x7f080006;
-        public static final int fragment_remote_control_oneButton=0x7f080005;
         public static final int fragment_remote_control_selectedTextView=0x7f080002;
         public static final int fragment_remote_control_tableLayout=0x7f080001;
         public static final int fragment_remote_control_workingTextView=0x7f080003;
-        public static final int fragment_remote_control_zeroButton=0x7f080004;
     }
     public static final class layout {
         public static final int activity_fragment=0x7f030000;
-        public static final int fragment_remote_control=0x7f030001;
+        public static final int button_row=0x7f030001;
+        public static final int fragment_remote_control=0x7f030002;
     }
     public static final class menu {
         public static final int remote_control=0x7f070000;
@@ -72,5 +70,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        /**  自定义Button样式 
+         */
+        public static final int RemoteButton=0x7f060002;
     }
 }
--- src/com/sunny/remotecontrol/RemoteControlFragment.java
+++ src/com/sunny/remotecontrol/RemoteControlFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class RemoteControlFragment extends Fragment {
@@ -39,16 +41,39 @@ public class RemoteControlFragment extends Fragment {
 			}
 		};
 		
-		Button zeroButton = (Button) view
-				.findViewById(R.id.fragment_remote_control_zeroButton);
+		// 动态设置Button的属性
+		TableLayout tableLayout = (TableLayout) view
+				.findViewById(R.id.fragment_remote_control_tableLayout);
+		int number = 1;
+		for (int i = 2; i < tableLayout.getChildCount(); i++) {
+			TableRow row = (TableRow) tableLayout.getChildAt(i);
+			for (int j = 0; j < row.getChildCount(); j++) {
+				Button button = (Button) row.getChildAt(j);
+				button.setText("" + number);
+				button.setOnClickListener(numberButtonListener);
+				number++;
+			}
+		}
+		
+		TableRow bottomRow = (TableRow) tableLayout
+				.getChildAt(tableLayout.getChildCount() - 1);
+		
+		Button deleteButton = (Button) bottomRow.getChildAt(0);
+		deleteButton.setText("Delete");
+		deleteButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mWorkingTextView.setText("0");
+			}
+		});
+		
+		Button zeroButton = (Button) bottomRow.getChildAt(1);
+		zeroButton.setText("0");
 		zeroButton.setOnClickListener(numberButtonListener);
 		
-		Button oneButton = (Button) view
-				.findViewById(R.id.fragment_remote_control_oneButton);
-		oneButton.setOnClickListener(numberButtonListener);
-		
-		Button enterButton = (Button) view
-				.findViewById(R.id.fragment_remote_control_enterButton);
+		Button enterButton = (Button) bottomRow.getChildAt(2);
+		enterButton.setText("Enter");
 		enterButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override

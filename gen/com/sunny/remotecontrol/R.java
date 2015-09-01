f094e3e527b7c18e59c33f6cfe472e2b85fbfe5f
Zhou Zejin <zjzhou1@chinaums.com>
2015-09-01 13:59:18 +0800

    完成第25章 XML Drawable与9-Patches

--- gen/com/sunny/remotecontrol/R.java
+++ gen/com/sunny/remotecontrol/R.java
@@ -22,7 +22,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bar=0x7f020000;
+        public static final int bar_patch=0x7f020001;
+        public static final int button_shape=0x7f020002;
+        public static final int button_shape_normal=0x7f020003;
+        public static final int button_shape_pressed=0x7f020004;
+        public static final int button_shape_shadowed=0x7f020005;
+        public static final int button_text_color=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int remote_background=0x7f020008;
+        public static final int window=0x7f020009;
+        public static final int window_patch=0x7f02000a;
     }
     public static final class id {
         public static final int action_settings=0x7f080004;
